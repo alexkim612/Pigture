e89bc545e36ddce13a72ab35e71f243316dc449a
Alex Kim <alexkim612@gmail.com>
2020-03-01 20:40:21 -0800

    pig length and reference

--- client/src/components/front-pig.jsx
+++ client/src/components/front-pig.jsx
@@ -7,13 +7,19 @@ class FrontPig extends React.Component {
 
     this.state = {
       selectedFile: '',
-      setPoints: null,
-      firstPoint: {x: 0, y: 0},
-      secondPoint: {x: 0, y: 0},
+      refOrPig: 'reference',
+      refSetPoints: null,
+      refFirstPoint: {x: 0, y: 0},
+      refSecondPoint: {x: 0, y: 0},
       measurement: 0,
+      pigSetPoints: null,
+      pigFirstPoint: {x: 0, y: 0},
+      pigSecondPoint: {x: 0, y: 0},
+      pigThirdPoint: {x: 0, y: 0},
     }
     this.fileUploaded = this.fileUploaded.bind(this);
     this.setPoint = this.setPoint.bind(this);
+    this.handleRefOrPig = this.handleRefOrPig.bind(this);
     this.handleMeasurementChange = this.handleMeasurementChange.bind(this);
     this.getInches = this.getInches.bind(this);
   }
@@ -26,21 +32,47 @@ class FrontPig extends React.Component {
   }
 
   setPoint(e) {
-    if (this.state.setPoints === 'first') {
+    if (this.state.refSetPoints === 'first') {
       this.setState({ 
-        firstPoint: {
+        refFirstPoint: {
           x: e.nativeEvent.offsetX, 
           y: e.nativeEvent.offsetY 
         },
-        setPoints: 'second',
+        refSetPoints: 'second',
       });
-    } else if (this.state.setPoints === 'second') {
+    } else if (this.state.refSetPoints === 'second') {
       this.setState({ 
-        secondPoint: {
+        refSecondPoint: {
           x: e.nativeEvent.offsetX, 
           y: e.nativeEvent.offsetY 
         },
-        setPoints: null,
+        refSetPoints: null,
+      });
+    }
+
+    if (this.state.pigSetPoints === 'first') {
+      this.setState({ 
+        pigFirstPoint: {
+          x: e.nativeEvent.offsetX, 
+          y: e.nativeEvent.offsetY 
+        },
+        pigSetPoints: 'second',
+      });
+    } else if (this.state.pigSetPoints === 'second') {
+      this.setState({ 
+        pigSecondPoint: {
+          x: e.nativeEvent.offsetX, 
+          y: e.nativeEvent.offsetY 
+        },
+        pigSetPoints: 'third',
+      });
+    } else if (this.state.pigSetPoints === 'third') {
+      this.setState({ 
+        pigSecondPoint: {
+          x: e.nativeEvent.offsetX, 
+          y: e.nativeEvent.offsetY 
+        },
+        pigSetPoints: null,
       });
     }
   }
@@ -52,9 +84,15 @@ class FrontPig extends React.Component {
   }
 
   getInches() {
-    const x = Math.abs(this.state.firstPoint.x - this.state.secondPoint.x);
-    const y = Math.abs(this.state.firstPoint.y - this.state.secondPoint.y);
+    const x = Math.abs(this.state.refFirstPoint.x - this.state.refSecondPoint.x);
+    const y = Math.abs(this.state.refFirstPoint.y - this.state.refSecondPoint.y);
     return (Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) / this.state.measurement);
+  }
+
+  handleRefOrPig() {
+    this.setState({
+      refOrPig: event.target.name,
+    });
   }
 
   render() {
@@ -66,15 +104,24 @@ class FrontPig extends React.Component {
             <img ref="image" id='front-pig-picture-container' src={this.state.selectedFile} onClick={this.setPoint}/>
           </div>
         </div>
+        <button name='reference' onClick={this.handleRefOrPig}>Reference Measurement</button>
+        <button name='pig' onClick={this.handleRefOrPig}>Pig Length</button>
+        {this.state.refOrPig === 'reference' ? 
         <div>
-          <label htmlFor='base-measurement'>Base Reference Measurement (inches)</label>
+          <h4>Reference Measurement</h4>
+          <label htmlFor='base-measurement'>Base Reference (inches)</label>
           <input type='number' onChange={this.handleMeasurementChange} className='base-measurement'/>
+          <button onClick={() => this.setState({refSetPoints:'first'})}>Set First Point</button>
+          <button onClick={() => this.setState({refSetPoints:'second'})}>Set Second Point</button>
         </div>
-          <button onClick={() => this.setState({setPoints:'first'})}>Set First Point</button>
-          <button onClick={() =>this.setState({setPoints:'second'})}>Set Second Point</button>
+        :
         <div>
-          <button>Draw Pig Length</button>
+          <h4>Pig Length</h4>
+          <button onClick={() => this.setState({pigSetPoints:'first'})}>Set First Point</button>
+          <button onClick={() => this.setState({pigSetPoints:'second'})}>Set Second Point</button>
+          <button onClick={() => this.setState({pigSetPoints:'third'})}>Set Third Point</button>
         </div>
+      }
       </div>
     );
   }
