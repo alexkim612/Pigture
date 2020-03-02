70cb5eb32c2f4e239a22654ecbb4fd91a6e377cf
Alex Kim <alexkim612@gmail.com>
2020-03-01 19:39:32 -0800

    getting points

--- client/src/components/front-pig.jsx
+++ client/src/components/front-pig.jsx
@@ -7,8 +7,12 @@ class FrontPig extends React.Component {
 
     this.state = {
       selectedFile: '',
+      setPoints: null,
+      firstPoint: {x: 0, y: 0},
+      secondPoint: {x: 0, y: 0},
     }
     this.fileUploaded = this.fileUploaded.bind(this);
+    this.setPoint = this.setPoint.bind(this);
   }
 
   fileUploaded(event) {
@@ -18,14 +22,44 @@ class FrontPig extends React.Component {
     console.log(event.target.files[0])
   }
 
+  setPoint(e) {
+    console.log(e);
+    if (this.state.setPoints === 'first') {
+      this.setState({ 
+        firstPoint: {
+          x: e.nativeEvent.offsetX, 
+          y: e.nativeEvent.offsetY 
+        },
+        setPoints: 'second',
+      });
+    } else if (this.state.setPoints === 'second') {
+      this.setState({ 
+        secondPoint: {
+          x: e.nativeEvent.offsetX, 
+          y: e.nativeEvent.offsetY 
+        },
+        setPoints: null,
+      });
+    }
+  }
+
   render() {
     return (
       <div className='front-pig-container'>
         <div className='pig-picture'>
           <input type='file' onChange={this.fileUploaded}/>
           <div className='picture-wrapper'>
-            <img id='front-pig-picture-container' src={this.state.selectedFile}/>
+            <img ref="image" id='front-pig-picture-container' src={this.state.selectedFile} onClick={this.setPoint}/>
           </div>
+        </div>
+        <div>
+          <label htmlFor='base-measurement'>Base Reference Measurement (inches)</label>
+          <input type='text' className='base-measurement'/>
+          <button onClick={() => this.setState({setPoints:'first'})}>Set First Point</button>
+          <button onClick={() =>this.setState({setPoints:'second'})}>Set Second Point</button>
+        </div>
+        <div>
+          <button>Draw Pig Length</button>
         </div>
       </div>
     );
