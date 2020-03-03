7e381cd2e606627916f46c153a79e9af9f005c5e
Alex Kim <alexkim612@gmail.com>
2020-03-03 14:46:26 -0800

    Some css styling

--- client/src/components/results.jsx
+++ client/src/components/results.jsx
@@ -30,6 +30,9 @@ function Results(props) {
           Shoulder: {(props.weight*0.72*0.08).toFixed(2)}lbs
         </div>
       </div>
+      <div className='pig-gif-container'>
+        <img className='pig-gif' src="https://cdn.clipart.email/10093347c236cd9b9d9a81fe5596102a_gif-parade_442-442.gif"/>
+      </div>
     </div>
   );
 }
