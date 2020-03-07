aa2469b691d8ca583d56b0fd0010d6b072f4f725
Alex Kim <alexkim612@gmail.com>
2020-03-07 11:31:08 -0800

    dropzone added

--- client/src/components/front-pig.jsx
+++ client/src/components/front-pig.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Dropzone from 'react-dropzone';
 
 class FrontPig extends React.Component {
   constructor(props) {
@@ -9,12 +10,12 @@ class FrontPig extends React.Component {
       selectedFile: '',
       refOrPig: 'reference',
       refSetPoints: null,
-      refFirstPoint: {x: 0, y: 0},
-      refSecondPoint: {x: 0, y: 0},
+      refFirstPoint: { x: 0, y: 0 },
+      refSecondPoint: { x: 0, y: 0 },
       measurement: 0,
       pigSetPoints: null,
-      pigCenterPoint: {x: 0, y: 0},
-      pigOuterPoint: {x: 0, y: 0},
+      pigCenterPoint: { x: 0, y: 0 },
+      pigOuterPoint: { x: 0, y: 0 },
     }
 
     this.fileUploaded = this.fileUploaded.bind(this);
@@ -36,34 +37,34 @@ class FrontPig extends React.Component {
   // Set Points
   setPoint(e) {
     if (this.state.refSetPoints === 'first') {
-      this.setState({ 
+      this.setState({
         refFirstPoint: {
-          x: e.nativeEvent.offsetX, 
-          y: e.nativeEvent.offsetY 
+          x: e.nativeEvent.offsetX,
+          y: e.nativeEvent.offsetY
         },
       });
     } else if (this.state.refSetPoints === 'second') {
-      this.setState({ 
+      this.setState({
         refSecondPoint: {
-          x: e.nativeEvent.offsetX, 
-          y: e.nativeEvent.offsetY 
+          x: e.nativeEvent.offsetX,
+          y: e.nativeEvent.offsetY
         },
         refSetPoints: null,
       });
     }
 
     if (this.state.pigSetPoints === 'center') {
-      this.setState({ 
+      this.setState({
         pigCenterPoint: {
-          x: e.nativeEvent.offsetX, 
-          y: e.nativeEvent.offsetY 
+          x: e.nativeEvent.offsetX,
+          y: e.nativeEvent.offsetY
         },
       });
     } else if (this.state.pigSetPoints === 'outer') {
-      this.setState({ 
+      this.setState({
         pigOuterPoint: {
-          x: e.nativeEvent.offsetX, 
-          y: e.nativeEvent.offsetY 
+          x: e.nativeEvent.offsetX,
+          y: e.nativeEvent.offsetY
         },
         pigSetPoints: null,
       });
@@ -105,31 +106,43 @@ class FrontPig extends React.Component {
     return (
       <div className='pig-container'>
         <div className='pig-picture'>
-          <input type='file' onChange={this.fileUploaded}/>
-          <div className='picture-wrapper'>
-            <img ref="image" id='pig-picture-container' src={this.state.selectedFile} onClick={this.setPoint}/>
+          {/* <input type='file' onChange={this.fileUploaded} /> */}
+          <div className='picture-wrapper' className='dropzone'>
+            {/* dropzone */}
+            <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+              {({ getRootProps, getInputProps }) => (
+                <section>
+                  <div {...getRootProps()}>
+                    <input {...getInputProps()} />
+                    <p>Drag 'n' drop some files here, or click to select files</p>
+                  </div>
+                </section>
+              )}
+            </Dropzone>
+
+            <img ref="image" id='pig-picture-container' src={this.state.selectedFile} onClick={this.setPoint} />
           </div>
         </div>
         <button name='reference' onClick={this.handleRefOrPig}>Reference Measurement</button>
         <button name='pig' onClick={this.handleRefOrPig}>Pig Heart Girth</button>
 
-        {this.state.refOrPig === 'reference' ? 
-        <div>
-          <h4>Reference Measurement</h4>
-          <label htmlFor='base-measurement'>Base Reference (inches)</label>
-          <input type='number' onChange={this.handleMeasurementChange} className='base-measurement'/>
-          <button onClick={() => this.setState({refSetPoints:'first'})}>Set First Point</button>
-          <button onClick={() => this.setState({refSetPoints:'second'})}>Set Second Point</button>
-        </div>
-        :
-        <div>
-          <h4>Pig Heart Girth</h4>
-          <button onClick={() => this.setState({pigSetPoints:'center'})}>Set Center Point</button>
-          <button onClick={() => this.setState({pigSetPoints:'outer'})}>Set Outer Point</button>
-        </div>
-      }
+        {this.state.refOrPig === 'reference' ?
+          <div>
+            <h4>Reference Measurement</h4>
+            <label htmlFor='base-measurement'>Base Reference (inches)</label>
+            <input type='number' onChange={this.handleMeasurementChange} className='base-measurement' />
+            <button onClick={() => this.setState({ refSetPoints: 'first' })}>Set First Point</button>
+            <button onClick={() => this.setState({ refSetPoints: 'second' })}>Set Second Point</button>
+          </div>
+          :
+          <div>
+            <h4>Pig Heart Girth</h4>
+            <button onClick={() => this.setState({ pigSetPoints: 'center' })}>Set Center Point</button>
+            <button onClick={() => this.setState({ pigSetPoints: 'outer' })}>Set Outer Point</button>
+          </div>
+        }
 
-      <button onClick={this.handleSubmit}>Submit</button>
+        <button onClick={this.handleSubmit}>Submit</button>
       </div>
     );
   }
